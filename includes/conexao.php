0dd33a2f262d052131723eccb79a764614703c1e
Fabio Lino Baroni <lino@uncle.com.br>
2020-11-30 17:52:47 -0300

    Add files via upload

--- includes/conexao.php
+++ includes/conexao.php
@@ -14,10 +14,10 @@
    }
    
 
-mysql_query("SET NAMES 'utf8'");
-    mysql_query('SET character_set_connection=utf8');
-    mysql_query('SET character_set_client=utf8');
-    mysql_query('SET character_set_results=utf8');
+    mysqli_set_charset("utf8");
+    mysqli_set_charset('SET character_set_connection=utf8');
+    mysqli_set_charset('SET character_set_client=utf8');
+    mysqli_set_charset('SET character_set_results=utf8');
 
 /*
 
