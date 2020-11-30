33685793d41e5695986f4b7f4f5935b522b5d7f9
Fabio Lino Baroni <lino@uncle.com.br>
2020-11-30 18:26:07 -0300

    Add files via upload

--- menu1.php
+++ menu1.php
@@ -2,46 +2,11 @@
    
      session_start(); // sempre que usarmos as sessions devemos chamar esse codigo sempre no inicio do script
 
-if(isset($_SESSION['login'])){// verifica se existe a varavel session
+
 
    $login=$_SESSION['login']; // passa o valor da variavel session para outra variavel so que uma variavel dentro do mesmo arquivo
    
 echo $login;
 
 
-?>
-
-<?php include 'includes/header.php';?>
-
-
-   
-
-		
-
-
-
-        
-
-
-
- <?php  include 'includes/footer.php';?>
-
-
- <?
-
-}
-else
-{
-//exiba um alerta dizendo que a senha esta errada
-?>
-
-<script type="text/javascript">
-alert("Por favor, efetue o login para acessar esse link")
-</script>
-
-<?
-echo "<div align='center'>";
-echo "<span class='style2'>Se voc&ecirc; j&aacute; tem cadastro volte a home e fa&ccedil;a login.<a href=index.php>VOLTAR A HOME</a></span>";
-echo "</div>";
-}
 ?>
